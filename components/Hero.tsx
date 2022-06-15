1c7b8c3972b54b32edf32ec9200ff6f2ae897d60
AayushMohan <aayushmohan1702@gmail.com>
2022-06-15 20:05:13 +0530

    Added Hero

---
+++ components/Hero.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div>
+      <div className="flex flex-col items-center justify-center text-left p-10 ">
+        <h1 className="text-3xl font-bold">Hey There 👋🏻, I'm Aayush Mohan</h1>
+        <p className="py-4">
+          I'm a Frontend Web Developer with a passion for outstanding design and
+          technology. I'm extremely enthusiastic about design and carrying it
+          through to completion. I'm a big fan of science and technology, so I
+          strive to stay on top of things so that our consumers get the greatest
+          experience possible.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
