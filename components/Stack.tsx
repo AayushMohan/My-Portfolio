430f2daffa13039172a30b2d0dd8077a1db2b1a4
AayushMohan <aayushmohan1702@gmail.com>
2022-06-24 21:15:04 +0530

    Added Programmer Img

--- components/Stack.tsx
+++ components/Stack.tsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
 import React from "react";
+import Coding from "../Assets/Coding.svg";
 
 const Stack = () => {
   return (
-    <div className="bg-inherit relative">
-      <div></div>
+    <div className="bg-inherit relative flex ">
+      <div>
+        <Image src={Coding} />
+      </div>
     </div>
   );
 };
