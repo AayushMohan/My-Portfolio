5ef31e70dc05497ebfef98852ab457a56f1c1606
AayushMohan <aayushmohan1702@gmail.com>
2022-06-24 21:30:54 +0530

    Added SVG Icons

--- components/Stack.tsx
+++ components/Stack.tsx
@@ -4,10 +4,11 @@ import Coding from "../Assets/Coding.svg";
 
 const Stack = () => {
   return (
-    <div className="bg-inherit relative flex ">
+    <div className="bg-inherit relative flex items-center justify-between">
       <div>
-        <Image src={Coding} />
+        <Image src={Coding} className="items-center" />
       </div>
+      <div></div>
     </div>
   );
 };
