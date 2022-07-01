e8eda3d55876bdba65733eb6b1b6fc1e80072233
AayushMohan <aayushmohan1702@gmail.com>
2022-07-01 22:04:14 +0530

    Added Projects

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
+import Projects from "../components/Projects";
 import Stack from "../components/Stack";
 
 const Home: NextPage = () => {
@@ -14,6 +15,7 @@ const Home: NextPage = () => {
       <Header />
       <Hero />
       <Stack />
+      <Projects />
     </div>
   );
 };
