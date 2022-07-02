b6334daf9a297c2e1d55661965471234b6a2c878
AayushMohan <aayushmohan1702@gmail.com>
2022-07-02 05:35:11 +0530

    Added: Links

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -2,6 +2,17 @@ import Image from "next/image";
 import React from "react";
 import HoverVideoPlayer from "react-hover-video-player";
 import OS from "../Assets/OS.png";
+import ReactIcon from "../Assets/React.svg";
+import NodeIcon from "../Assets/Node.svg";
+import TypeScript from "../Assets/TypeScript.svg";
+import NextIcon from "../Assets/Next.svg";
+import Vercel from "../Assets/Vercel.svg";
+import Git from "../Assets/Git.svg";
+import Github from "../Assets/Github.svg";
+import Metamask from "../Assets/Metamask.svg";
+import Sanity from "../Assets/Sanity.svg";
+import ThirdWeb from "../Assets/ThirdWeb.svg";
+import Moralis from "../Assets/Moralis.svg";
 
 const Projects = () => {
   return (
@@ -9,13 +20,33 @@ const Projects = () => {
       <h1 className="text-6xl font-semibold text-center py-12">
         Featured Projects
       </h1>
-      <div className="overflow-hidden relative h-screen bg-inherit">
-        <HoverVideoPlayer
-          videoSrc="https://res.cloudinary.com/upwork-fp/video/upload/c_scale,w_1000,q_auto/v1647622694/profile/portfolio/1396689749490266112/sibjvk6arybl7tdsd6pt.mp4"
-          className="h-[40rem] w-[40rem] space-x-8"
-          pausedOverlay={<Image src={OS} />}
-        />
-        <h2>OpenSea Clone</h2>
+      <div className="overflow-hidden h-screen bg-inherit flex md:flex-row flex-col">
+        <div>
+          <HoverVideoPlayer
+            videoSrc="https://res.cloudinary.com/upwork-fp/video/upload/c_scale,w_1000,q_auto/v1647622694/profile/portfolio/1396689749490266112/sibjvk6arybl7tdsd6pt.mp4"
+            className="md:h-[40rem] md:w-[40rem] md:space-x-2 p-8 transition ease-in duration-150"
+            pausedOverlay={
+              <Image src={OS} width="1150" height="700" objectFit="fill" />
+            }
+          />
+        </div>
+        <div className="md:p-10 md:space-x-10 p-10">
+          <h2 className="text-3xl md:mx-40 md:text-right">OpenSea Clone</h2>
+          <p className="text-xl md:mx-40 md:my-4 md:text-right">
+            This site is a clone of Opensea, the first and largest NFT
+            marketplace in the world. Users can use Metamask to authenticate and
+            purchase samples from the collection.
+          </p>
+
+          <div className="h-14 w-14 my-4 cursor-pointer flex justify-start ">
+            <a href="https://reactjs.org/">
+              <Image src={ReactIcon} className="" />
+            </a>
+            <a href="https://nodejs.org/en/">
+              <Image src={NodeIcon} className="" />
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   );
--- components/Stack.tsx
+++ components/Stack.tsx
@@ -24,31 +24,69 @@ import Wave2 from "../Assets/Wave-2.svg";
 const Stack = () => {
   return (
     <div className="bg-inherit overflow-hidden ">
-      <h1 className="text-center font-bold text-3xl text-gray-200 p-10 hover:ease-in transition hover:-translate-y-4 duration-300 cursor-pointer">
+      <h1 className="text-center font-bold text-6xl text-gray-200 p-10 hover:ease-in transition hover:-translate-y-4 duration-300 cursor-pointer">
         My Tech Stack
       </h1>
       <div className="flex md:justify-between md:flex-row flex-col">
         <Image src={Coding} className="" />
-        <div className="grid grid-cols-6 gap-4 md:gap-6 p-6 md:mr-12 md:px-8 overflow-hidden h-[25rem] w-[25rem] md:h-[35rem] md:w-[35rem]">
+        <div className="grid md:grid-cols-6 grid-cols-6 gap-6 md:gap-6 p-4 md:mr-12 md:px-8 overflow-hidden h-[25rem] w-[25rem] md:h-[35rem] md:w-[35rem] items-center">
           {/* <div className=""> */}
-          <Image src={HTML} className="" />
-          <Image src={CSS} className="" />
-          <Image src={Bootstrap} className="" />
-          <Image src={SCSS} className="" />
-          <Image src={Javascript} className="" />
-          <Image src={ReactIcon} className="" />
-          <Image src={NodeIcon} className="" />
-          <Image src={TypeScript} className="" />
-          <Image src={NextIcon} className="" />
-          <Image src={Vercel} className="" />
-          <Image src={Git} className="" />
-          <Image src={Github} className="" />
-          <Image src={Metamask} className="" />
-          <Image src={Sanity} className="" />
-          <Image src={ThirdWeb} className="" />
-          <Image src={Moralis} className="" />
-          <Image src={FireBase} className="" />
-          <Image src={Alchemy} className="" />
+          <a href="https://www.w3schools.com/html/">
+            <Image src={HTML} className="" />
+          </a>
+          <a href="https://www.w3schools.com/css/">
+            <Image src={CSS} className="" />
+          </a>
+          <a href="https://getbootstrap.com/docs/5.0/getting-started/introduction/">
+            <Image src={Bootstrap} className="" />
+          </a>
+          <a href="https://sass-lang.com/">
+            <Image src={SCSS} className="" />
+          </a>
+          <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript">
+            <Image src={Javascript} className="" />
+          </a>
+          <a href="https://reactjs.org/">
+            <Image src={ReactIcon} className="" />
+          </a>
+          <a href="https://nodejs.org/en/">
+            <Image src={NodeIcon} className="" />
+          </a>
+          <a href="https://www.typescriptlang.org/">
+            <Image src={TypeScript} className="" />
+          </a>
+          <a href="https://nextjs.org/">
+            <Image src={NextIcon} className="" />
+          </a>
+
+          <a href="https://vercel.com/home?utm_source=next-site&utm_medium=banner&utm_campaign=next-website">
+            <Image src={Vercel} className="" />
+          </a>
+          <a href="https://git-scm.com/">
+            <Image src={Git} className="" />
+          </a>
+
+          <a href="https://github.com/">
+            <Image src={Github} className="" />
+          </a>
+          <a href="https://metamask.io/">
+            <Image src={Metamask} className="" />
+          </a>
+          <a href="https://sanity.io/">
+            <Image src={Sanity} className="" />
+          </a>
+          <a href="https://thirdweb.com/">
+            <Image src={ThirdWeb} className="" />
+          </a>
+          <a href="https://moralis.io/">
+            <Image src={Moralis} className="" />
+          </a>
+          <a href="https://firebase.google.com/">
+            <Image src={FireBase} className="" />
+          </a>
+          <a href="https://alchemy.com/">
+            <Image src={Alchemy} className="" />
+          </a>
         </div>
       </div>
       <Image src={Wave2} className="" />
